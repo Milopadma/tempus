13adedae7dfd7778f2dd8ad6079e2570fabbb9c6
mylo <milopadma@yahoo.com>
2023-02-17 11:22:51 +0800

    react-spring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { animated, useSpring } from "@react-spring/web";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  // react spring
+  const springs = useSpring({
+    from: { opacity: 0, transform: "translate3d(0, 400px, 0)" },
+    to: { opacity: 1, transform: "translate3d(0, 0px, 0)" },
+  });
+
   return (
-    <div className="App">
+    <div className="App flex-col space-x-16">
       <h1>what skill would you like to focus on?</h1>
-      <div>
-        
-      </div>
+      <animated.div style={springs} className="button-container">
+        <button onClick={() => setCount(count + 1)}>React</button>
+      </animated.div>
     </div>
   );
 }
